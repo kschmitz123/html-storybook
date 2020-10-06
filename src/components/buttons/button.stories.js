c434a5ee691d36715a5b27a1e54cbfd90c075f21
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 14:28:54 +0200

    Clean up folders

--- src/components/buttons/button.stories.js
+++ src/components/buttons/button.stories.js
@@ -1,22 +1,11 @@
 import "./button.css";
-import favButtonText from "./favoritesButton-text.html";
-import favButtonLogo from "./favoritesButton-logo.html";
-import favButtonLogoAndText from "./favoritesButton-logo-and-text.html";
-import playButtonText from "./playButton-text.html";
-import playButtonLogo from "./playButton-logo.html";
-import playButtonLogoAndText from "./playButton-logo-and-text.html";
-import shareButtonText from "./shareButton-text.html";
-import shareButtonLogo from "./shareButton-logo.html";
-import shareButtonLogoAndText from "./shareButton-logo-and-text.html";
+
+import favButton from "./favoritesButton.html";
+import playButton from "./playButton.html";
+import shareButton from "./shareButton.html";
 
 export default { title: "Components/Buttons" };
 
-export const withFavText = () => favButtonText;
-export const withFavLogo = () => favButtonLogo;
-export const withFavLogoAndText = () => favButtonLogoAndText;
-export const withText = () => playButtonText;
-export const withLogo = () => playButtonLogo;
-export const withLogoAndText = () => playButtonLogoAndText;
-export const withShareText = () => shareButtonText;
-export const withShareLogo = () => shareButtonLogo;
-export const withShareLogoAndText = () => shareButtonLogoAndText;
+export const FavButton = () => favButton;
+export const PlayButton = () => playButton;
+export const ShareButton = () => shareButton;
