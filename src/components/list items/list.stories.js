d002a1c5c8f400811ec38b6679123806362c113a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 15:55:27 +0200

    Change design

--- src/components/list items/list.stories.js
+++ src/components/list items/list.stories.js
@@ -2,16 +2,10 @@ import "./list.css";
 
 import themes from "./li-themes.html";
 import ringtoneCutter from "./li-ringtone-cutter.html";
-import driveMode from "./li-drive-mode.html";
-import equaliser from "./li-equaliser.html";
-import hiddenFolders from "./li-hidden-folders.html";
-import sleepTimer from "./li-sleep-timer.html";
+import listMenu from "./list-menu.html";
 
 export default { title: "Components/List items" };
 
 export const Themes = () => themes;
 export const RingtoneCutter = () => ringtoneCutter;
-export const DriveMode = () => driveMode;
-export const Equaliser = () => equaliser;
-export const HiddenFolders = () => hiddenFolders;
-export const SleepTimer = () => sleepTimer;
+export const ListMenu = () => listMenu;
