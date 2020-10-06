20ed803247a4232e580ccf9e7e9223bec6b96be1
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 20:44:06 +0200

    Add player icon list

--- src/components/player icons/playericons.stories.js
+++ src/components/player icons/playericons.stories.js
@@ -1,8 +1,10 @@
 import "./playericons.css";
 import playlist from "./playlist.html";
 import shuffle from "./shuffle.html";
+import playerIconList from "./playericon-list.html";
 
 export default { title: "Components/Player Icons" };
 
 export const Playlist = () => playlist;
 export const Shuffle = () => shuffle;
+export const PlayerIconList = () => playerIconList;
