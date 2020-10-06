c71c545f846636aa7d546b05d966c9b4405b4ec1
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 15:23:15 +0200

    Add more files

--- src/components/list items/list.stories.js
+++ src/components/list items/list.stories.js
@@ -2,8 +2,16 @@ import "./list.css";
 
 import themes from "./li-themes.html";
 import ringtoneCutter from "./li-ringtone-cutter.html";
+import driveMode from "./li-drive-mode.html";
+import equaliser from "./li-equaliser.html";
+import hiddenFolders from "./li-hidden-folders.html";
+import sleepTimer from "./li-sleep-timer.html";
 
 export default { title: "Components/List items" };
 
 export const Themes = () => themes;
 export const RingtoneCutter = () => ringtoneCutter;
+export const DriveMode = () => driveMode;
+export const Equaliser = () => equaliser;
+export const HiddenFolders = () => hiddenFolders;
+export const SleepTimer = () => sleepTimer;
