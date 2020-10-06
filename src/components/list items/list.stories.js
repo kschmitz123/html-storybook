1e25f1a47c24152852ee7b798cacfa8d9ac0bd6b
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 14:49:21 +0200

    Add list files

---
+++ src/components/list items/list.stories.js
@@ -0,0 +1,9 @@
+import "./list.css";
+
+import themes from "./li-themes.html";
+import ringtoneCutter from "./li-ringtone-cutter.html";
+
+export default { title: "Components/List items" };
+
+export const Themes = () => themes;
+export const RingtoneCutter = () => ringtoneCutter;
