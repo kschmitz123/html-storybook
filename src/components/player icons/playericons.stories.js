5fd9a0435a03b4ab923c965685e800139571c6f4
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-06 20:25:52 +0200

    Add playlist and shuffle button

---
+++ src/components/player icons/playericons.stories.js
@@ -0,0 +1,8 @@
+import "./playericons.css";
+import playlist from "./playlist.html";
+import shuffle from "./shuffle.html";
+
+export default { title: "Components/Player Icons" };
+
+export const Playlist = () => playlist;
+export const Shuffle = () => shuffle;
