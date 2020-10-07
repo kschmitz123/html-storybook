641c320713dd10d12680eafcb8edddae21d843b1
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-07 16:23:41 +0200

    Fix centered issue

--- src/components/buttons/button.stories.js
+++ src/components/buttons/button.stories.js
@@ -4,8 +4,9 @@ import favButton from "./favoritesButton.html";
 import playButton from "./playButton.html";
 import shareButton from "./shareButton.html";
 import followButton from "./followButton.html";
+import centered from "@storybook/addon-centered/html";
 
-export default { title: "Components/Buttons" };
+export default { title: "Components/Buttons", decorators: [centered] };
 
 export const FavButton = () => favButton;
 export const PlayButton = () => playButton;
--- src/components/player icons/playericons.stories.js
+++ src/components/player icons/playericons.stories.js
@@ -2,8 +2,9 @@ import "./playericons.css";
 import playlist from "./playlist.html";
 import shuffle from "./shuffle.html";
 import playerIconList from "./playericon-list.html";
+import centered from "@storybook/addon-centered/html";
 
-export default { title: "Components/Player Icons" };
+export default { title: "Components/Player Icons", decorators: [centered] };
 
 export const Playlist = () => playlist;
 export const Shuffle = () => shuffle;
