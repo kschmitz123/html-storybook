49b76d091bd9f01ab9a37300d63f760324848841
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-07 14:38:51 +0200

    Fix position

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,7 +3,7 @@ import "@storybook/addon-console";
 import centered from "@storybook/addon-centered/html";
 import "../src/global.css";
 
-// addDecorator(centered);
+addDecorator(centered);
 addParameters({
   a11y: {
     config: {},
