7618a3062315416feb96fee34bc65bd17fff066e
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-08 16:53:29 +0200

    Adapt layout listmenu

--- src/pages/settings/settings.stories.js
+++ src/pages/settings/settings.stories.js
@@ -1,4 +1,7 @@
 import "./settings.css";
+import "../../components/page-layout/page-layout.css";
+import "../../components/list items/list.css";
+
 import settings from "./settings.html";
 
 export default { title: "Pages/Settings" };
