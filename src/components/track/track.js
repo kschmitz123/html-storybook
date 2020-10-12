ca0119071c9562eb3c1f6f8813b16feb630100ee
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-12 14:45:28 +0200

    Add javascript

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -1,17 +1,29 @@
+import "./track.css";
+import billieJean from "../../assets/billie-jean.png";
+import playBtnActive from "../../assets/playbutton-active.svg";
+
 export function createTrackElement() {
+  const title = "Billie Jean";
+  const artist = "Michael Jackson";
+
   const divElement = document.createElement("div"); // <div></div>
 
   const h3Element = document.createElement("h3"); // <h3></h3>
-  h3Element.innerText = "Billie Jean"; // <h3>Billie Jean</h3>
+  h3Element.innerText = title; // <h3>Billie Jean</h3>
 
   const pElement = document.createElement("p"); // create <p>
-  pElement.innerText = "Michael Jackson"; // create <p>Michael Jackson<p>
+  pElement.innerText = artist; // create <p>Michael Jackson<p>
 
-  const button = document.createElement("button");
+  const btnElement = document.createElement("button"); // <button>
+  const playActionElement = document.createElement("img");
+  playActionElement.src = playBtnActive;
 
-  const img = document.createElement("img");
+  const imgElement = document.createElement("img"); // <img>
+  imgElement.src = billieJean;
+  imgElement.alt = "Image of " + artist;
 
-  divElement.append(img, h3Element, pElement, button);
+  divElement.append(imgElement, h3Element, pElement, btnElement);
+  btnElement.append(playActionElement);
 
   return divElement;
 }
