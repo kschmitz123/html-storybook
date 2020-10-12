ae503c82bca1ad67d09a815db30f15e29e4cda5c
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-12 15:36:37 +0200

    Add styling

--- src/components/track/track.js
+++ src/components/track/track.js
@@ -2,28 +2,49 @@ import "./track.css";
 import billieJean from "../../assets/billie-jean.png";
 import playBtnActive from "../../assets/playbutton-active.svg";
 
+/*
+<div class="track">
+    <img class="track__image"/>
+    <div class="track__info">
+    <h3 class="track__title">
+    <p class="track__artist">
+    </div>
+    <button class="playbutton">
+        <img>
+    </button>
+</div>
+*/
+
 export function createTrackElement() {
   const title = "Billie Jean";
   const artist = "Michael Jackson";
 
-  const divElement = document.createElement("div"); // <div></div>
+  const trackElement = document.createElement("div"); // <div></div>
+  trackElement.className = "track";
 
-  const h3Element = document.createElement("h3"); // <h3></h3>
-  h3Element.innerText = title; // <h3>Billie Jean</h3>
+  const trackImage = document.createElement("img"); // <img>
+  trackImage.src = billieJean;
+  trackImage.alt = "Image of " + artist;
 
-  const pElement = document.createElement("p"); // create <p>
-  pElement.innerText = artist; // create <p>Michael Jackson<p>
+  const trackInfo = document.createElement("div");
+  trackInfo.className = "track__info";
+
+  const trackTitle = document.createElement("h3"); // <h3></h3>
+  trackTitle.innerText = title; // <h3>Billie Jean</h3>
+  trackTitle.className = "track__title";
+
+  const trackArtist = document.createElement("p"); // create <p>
+  trackArtist.innerText = artist; // create <p>Michael Jackson<p>
+  trackArtist.className = "track__artist";
 
   const btnElement = document.createElement("button"); // <button>
-  const playActionElement = document.createElement("img");
+  const playActionElement = document.createElement("img"); // <img>
   playActionElement.src = playBtnActive;
+  btnElement.className = "playbutton";
 
-  const imgElement = document.createElement("img"); // <img>
-  imgElement.src = billieJean;
-  imgElement.alt = "Image of " + artist;
-
-  divElement.append(imgElement, h3Element, pElement, btnElement);
+  trackElement.append(trackImage, trackInfo, btnElement);
   btnElement.append(playActionElement);
+  trackInfo.append(trackTitle, trackArtist);
 
-  return divElement;
+  return trackElement;
 }
