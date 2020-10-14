d167927902cdc20923ef36fb7d737b26d407b240
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-14 16:55:35 +0200

    Add array and songlist

--- src/components/track/track.stories.js
+++ src/components/track/track.stories.js
@@ -1,33 +1,71 @@
 import { createTrackElement } from "./track";
-import billieJean from "../../assets/billie-jean.png";
+import billieJeanImg from "../../assets/billie-jean.png";
 
 export default {
   title: "Components/Track",
   parameters: { layout: "centered" },
 };
 
-export const BillieJean = () =>
-  createTrackElement({
+// export const BillieJean = () =>
+//   createTrackElement({
+//     title: "Billie Jean",
+//     artist: "Michael Jackson",
+//     imgSrc: billieJean,
+//     audioSrc:
+//       "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_09_-_Back_on_the_road.mp3",
+//   });
+// export const EarthSong = () =>
+//   createTrackElement({
+//     title: "Earth Song",
+//     artist: "Michael Jackson",
+//     imgSrc:
+//       "https://i.pinimg.com/originals/2c/7d/99/2c7d99b45744abba70aea6be9127ea34.png",
+//     audioSrc:
+//       "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_03_-_Walking_on_water.mp3",
+//   });
+// export const SmoothCriminal = () =>
+//   createTrackElement({
+//     title: "Evening Journey",
+//     artist: "TRG Banks",
+//     imgSrc: "https://findicons.com/files/icons/480/weather/256/rainbow.png",
+//     audioSrc:
+//       "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_02_-_Evening_journey.mp3",
+//   });
+
+// TRACKLIST GENERIEREN
+export const songs = () => {
+  const eveningJourney = {
+    title: "Evening Journey",
+    artist: "TRG Banks",
+    imgSrc: "https://findicons.com/files/icons/480/weather/256/rainbow.png",
+    audioSrc:
+      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_02_-_Evening_journey.mp3",
+  };
+
+  const billieJean = {
     title: "Billie Jean",
     artist: "Michael Jackson",
-    imgSrc: billieJean,
+    imgSrc: billieJeanImg,
     audioSrc:
       "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_09_-_Back_on_the_road.mp3",
-  });
-export const EarthSong = () =>
-  createTrackElement({
+  };
+  const smoothCriminal = {
     title: "Earth Song",
     artist: "Michael Jackson",
     imgSrc:
       "https://i.pinimg.com/originals/2c/7d/99/2c7d99b45744abba70aea6be9127ea34.png",
     audioSrc:
       "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_03_-_Walking_on_water.mp3",
+  };
+
+  const tracklist = [eveningJourney, billieJean, smoothCriminal];
+
+  const container = document.createElement("div");
+
+  tracklist.forEach((track) => {
+    const trackElement = createTrackElement(track);
+    container.append(trackElement);
   });
-export const SmoothCriminal = () =>
-  createTrackElement({
-    title: "Evening Journey",
-    artist: "TRG Banks",
-    imgSrc: "https://findicons.com/files/icons/480/weather/256/rainbow.png",
-    audioSrc:
-      "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/TRG_Banks/The_Rainbows_End/TRG_Banks_-_02_-_Evening_journey.mp3",
-  });
+
+  return container;
+};
